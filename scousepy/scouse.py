91c33c70ec4d592cd645b883a31f4f3d3531b05c
jdhenshaw <jonathan.d.henshaw@gmail.com>
2017-11-23 15:19:04 +0100

    initial layout scousepy

---
+++ scousepy/scouse.py
@@ -0,0 +1,13 @@
+"""
+
+SCOUSE - Semi-automated multi-COmponent Universal Spectral-line fitting Engine
+Copyright (c) 2017 Jonathan D. Henshaw
+CONTACT: j.d.henshaw[AT]ljmu.ac.uk
+
+"""
+
+class scouse(object):
+
+    def __init__(self):
+
+        self.data = None
