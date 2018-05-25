678ec0e7da8cc15f0b1a4461c059ac7a4f97e9fc
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-05-25 18:06:00 +0200

    add some documentation to the iterative fit script

--- examples/run_iterative.py
+++ examples/run_iterative.py
@@ -38,7 +38,7 @@ cube = SpectralCube.read('n2h+10_37.fits').with_spectral_unit(u.km/u.s)
 dscube = cube.spatial_smooth(Box2DKernel(2))[:,::2,::2]
 dscube.write('n2h+10_37_ds.fits', overwrite=True)
 
-#s = scouse.stage_1(filename, datadirectory, ppv_vol, rsaa, mask_below=0.3, verbose = verb, training_set=TS, samplesize=1, write_moments=True, save_fig=True)
+# run the full scouse suite on the downsampled data
 if os.path.exists(datadirectory+filename+'/stage_1/s1.scousepy'):
     s = scouse(outputdir=datadirectory, filename=filename, fittype=fittype,
                datadirectory=datadirectory)
@@ -72,24 +72,31 @@ if os.path.exists(datadirectory+filename+'/stage_6/s6.scousepy'):
 else:
     s = scouse.stage_6(s, plot_neighbours=True, radius_pix = 2, figsize = [18,10], plot_residuals=True, write_ascii=True, verbose=verb)
 
+# now the downsampled data are done, so we'll expand the guesses to all pixels
+# and do the automatic fitting steps
 
+# first, we get the linear (flat) indices of each pixel in the full-resolution data
 index_shape = np.arange(cube[0,:,:].size).reshape(cube.shape[1:])
 
+# create Spectral Averaging Areas from each of the fitted spectra in the low-res data
 from scousepy.saa_description import saa
 new_saa = {ind: saa.from_indiv_spectrum(spec, scouse=s, sample=True)
            for ind,spec in s.indiv_dict.items()}
 for ind,SAA in new_saa.items():
+    # x,y coordinates were 1/2, so multiply them
     SAA._coordinates *= 2
     cy,cx = SAA.coordinates
+    # fill in the flat indices.  _indices_flat is how the SAA and indiv spectra are associated
     SAA._indices_flat = [index_shape[y,x] for (y,x) in [(cy,cx), (cy+1, cx), (cy, cx+1), (cy+1, cx+1)]
                          if y<index_shape.shape[0] and x<index_shape.shape[1]]
+    # populate the various models from the fitted dict
     SAA._model = s.indiv_dict[ind]._model
     SAA._models = s.indiv_dict[ind]._models
     SAA._model_dud = s.indiv_dict[ind]._model_dud
     SAA._model_parent = s.indiv_dict[ind]._model_parent
     SAA._model_spatial = s.indiv_dict[ind]._model_spatial
 
-
+# change parameters back to the full-resolution version
 filename         =  'n2h+10_37'
 s.filename = filename
 s.outputdirectory = './n2h+10_37'
@@ -97,6 +104,23 @@ s.cube = cube
 s.saa_dict = {0: new_saa}
 s.indiv_dict = {}
 
-s = scouse.stage_3(s, tol, njobs=njobs, verbose=True)
-s = scouse.stage_4(s, verbose=verb)
-s = scouse.stage_5(s, blocksize = 6, figsize = [18,10], plot_residuals=True, verbose=verb)
+# run stages 3-6 with the high-res data
+if os.path.exists(datadirectory+filename+'/stage_3/s3.scousepy'):
+    s.load_stage_3(datadirectory+filename+'/stage_3/s3.scousepy')
+else:
+    s = scouse.stage_3(s, tol, njobs=njobs, verbose=verb)
+
+if os.path.exists(datadirectory+filename+'/stage_4/s4.scousepy'):
+    s.load_stage_4(datadirectory+filename+'/stage_4/s4.scousepy')
+else:
+    s = scouse.stage_4(s, verbose=verb)
+
+if os.path.exists(datadirectory+filename+'/stage_5/s5.scousepy'):
+    s.load_stage_5(datadirectory+filename+'/stage_5/s5.scousepy')
+else:
+    s = scouse.stage_5(s, blocksize = 6, figsize = [18,10], plot_residuals=True, verbose=verb)
+
+if os.path.exists(datadirectory+filename+'/stage_6/s6.scousepy'):
+    s.load_stage_6(datadirectory+filename+'/stage_6/s6.scousepy')
+else:
+    s = scouse.stage_6(s, plot_neighbours=True, radius_pix = 2, figsize = [18,10], plot_residuals=True, write_ascii=True, verbose=verb)
