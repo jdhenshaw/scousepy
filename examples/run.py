45ee685764913f3f936fa796057c52578c218628
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-05-25 14:02:40 +0200

    change example run script to work with new io methods

--- examples/run.py
+++ examples/run.py
@@ -30,27 +30,35 @@ def run_scousepy():
 
     #s = scouse.stage_1(filename, datadirectory, ppv_vol, rsaa, mask_below=0.3, verbose = verb, training_set=TS, samplesize=1, write_moments=True, save_fig=True)
     if os.path.exists(datadirectory+filename+'/stage_1/s1.scousepy'):
-        s = scouse.load_from(datadirectory+filename+'/stage_1/s1.scousepy')
+        s = scouse(outputdir=datadirectory, filename=filename, fittype=fittype,
+                   datadirectory=datadirectory)
+        s.load_stage_1(datadirectory+filename+'/stage_1/s1.scousepy')
+        s.load_cube(fitsfile=filename+".fits")
     else:
         s = scouse.stage_1(filename, datadirectory, ppv_vol, rsaa, mask_below=0.3, fittype=fittype, verbose = verb, refine_grid=RG, nrefine = nRG, write_moments=True, save_fig=True)
+
     if os.path.exists(datadirectory+filename+'/stage_2/s2.scousepy'):
-        s = scouse.load_from(datadirectory+filename+'/stage_2/s2.scousepy')
+        s.load_stage_2(datadirectory+filename+'/stage_2/s2.scousepy')
     else:
         s = scouse.stage_2(s, verbose=verb, write_ascii=True)
+
     if os.path.exists(datadirectory+filename+'/stage_3/s3.scousepy'):
-        s = scouse.load_from(datadirectory+filename+'/stage_3/s3.scousepy')
+        s.load_stage_3(datadirectory+filename+'/stage_3/s3.scousepy')
     else:
         s = scouse.stage_3(s, tol, njobs=njobs, verbose=verb)
+
     if os.path.exists(datadirectory+filename+'/stage_4/s4.scousepy'):
-        s = scouse.load_from(datadirectory+filename+'/stage_4/s4.scousepy')
+        s.load_stage_4(datadirectory+filename+'/stage_4/s4.scousepy')
     else:
         s = scouse.stage_4(s, verbose=verb)
+
     if os.path.exists(datadirectory+filename+'/stage_5/s5.scousepy'):
-        s = scouse.load_from(datadirectory+filename+'/stage_5/s5.scousepy')
+        s.load_stage_5(datadirectory+filename+'/stage_5/s5.scousepy')
     else:
         s = scouse.stage_5(s, blocksize = 6, figsize = [18,10], plot_residuals=True, verbose=verb)
+
     if os.path.exists(datadirectory+filename+'/stage_6/s6.scousepy'):
-        s = scouse.load_from(datadirectory+filename+'/stage_6/s6.scousepy')
+        s.load_stage_6(datadirectory+filename+'/stage_6/s6.scousepy')
     else:
         s = scouse.stage_6(s, plot_neighbours=True, radius_pix = 2, figsize = [18,10], plot_residuals=True, write_ascii=True, verbose=verb)
 
