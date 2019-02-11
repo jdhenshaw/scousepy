99312f13ec7cb3b33cb82a3648d8ddd877e9eda3
jdhenshaw <jonathan.d.henshaw@gmail.com>
2019-02-11 10:24:33 +0100

    updating documentation

--- docs/conf.py
+++ docs/conf.py
@@ -81,9 +81,9 @@ import sphinx_bootstrap_theme
 # ...
 
 # Activate the theme.
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-#html_theme = 'sphinx_rtd_theme'
+# html_theme = 'bootstrap'
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
