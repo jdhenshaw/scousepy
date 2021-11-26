48f6e25d283f0210ed5a487f7fe8491a564af355
jdhenshaw <jonathan.d.henshaw@gmail.com>
2021-11-26 15:39:58 +0100

    updating docs

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.dev338',
+    VERSION: '1.0.1.dev66+g38c657b.d20211126',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
