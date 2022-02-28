50fe366390a2dcf7d5244d490b3884e2dbeed2c3
jdhenshaw <jonathan.d.henshaw@gmail.com>
2022-02-28 17:18:49 +0100

    removing print statements

--- scousepy/SpectralDecomposer.py
+++ scousepy/SpectralDecomposer.py
@@ -142,10 +142,10 @@ class Decomposer(object):
         errors=np.asarray([np.nan if np.invert(np.isfinite(error)) else error for error in errors  ])
 
         if np.any(np.invert(np.isfinite(errors))):
-            print('initial fit did not converge...modifying initial guesses')
+            #print('initial fit did not converge...modifying initial guesses')
             guesses = np.copy(self.pskspectrum.specfit.modelpars)
             rounding = np.asarray([np.abs(np.floor(np.log10(guess))) if np.floor(np.log10(guess))<0.0 else 1.0 for guess in guesses])
-            print(rounding)
+            #print(rounding)
             self.guesses = np.asarray([np.around(guess,decimals=int(rounding[i])) for i, guess in enumerate(guesses)])
 
             nparams=np.size(self.pskspectrum.specfit.fitter.parnames)
